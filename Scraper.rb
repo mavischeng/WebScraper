ad35cac8400d89e9cbb3c212266f87a31d24ddb5
penguin <mavis801227@hotmail.com>
2014-10-11 14:13:57 +0800

    Change : Scraper.rb (No.3)

--- Scraper.rb
+++ Scraper.rb
@@ -4,8 +4,10 @@ require 'nokogiri'
 class WebScraper
   # @data stores information
   attr_accessor :data
+  attr_accessor :station
   def initialize
     @data = data
+    @station = station
   end
 
   def getwebstructure(website)
@@ -15,9 +17,10 @@ class WebScraper
   end
 
   def busstation
-    num = 1
+    num, @station = 1, []
     getwebstructure('http://www.hcbus.com.tw/big5/service.asp')
     @data.css("select[name='jumpMenu'] option").each do |x|
+      @station << x.text
       puts 'Station ' << num.to_s << ' : ' << x.text
       num += 1
     end
@@ -25,5 +28,8 @@ class WebScraper
 
   def selectstation
     busstation
+    puts 'Please select a station (1,2,3.....)'
+    select_station = $stdin.gets.chomp
+    puts (select_station.to_i >= 0 && select_station.to_i <= @station.length) ? station[select_station.to_i - 1] : '[ERROR] Please select provided number above.'
   end
 end
