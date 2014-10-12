0d11c64930a8dfc2d6348a0b8c5cdc2ce3c22994
erinerin <jolin_2121@hotmail.com.tw>
2014-10-12 20:16:20 +0800

    rubocop !!!

--- Scraper.rb
+++ Scraper.rb
@@ -1,4 +1,4 @@
-require 'open-uri'
+require 'open_uri'
 require 'nokogiri'
 require 'rubygems'
 require 'mechanize'
@@ -14,7 +14,7 @@ class WebScraper
     @data = data
     @station = station
     @page = page
-    @output=output
+    @output = output
   end
 
   def getwebstructure(website)
@@ -22,12 +22,13 @@ class WebScraper
     @data = Nokogiri.HTML(web_data)
     File.write('Structure.txt', @data)
   end
-  def file_output
-         selectstation
-      File.write('Output.txt', @output)
-puts "\n\n\nPlease check data in Output.txt file"
 
+  def file_output
+    selectstation
+    File.write('Output.txt', @output)
+    puts "\n\n\nPlease check data in Output.txt file"
   end
+
   def busstation
     num, @station = 1, []
     getwebstructure('http://www.hcbus.com.tw/big5/service.asp')
@@ -42,45 +43,54 @@ puts "\n\n\nPlease check data in Output.txt file"
     busstation
     puts 'Please select a region  (Enter:1 or 2 or 3.....)'
     select_station = $stdin.gets.chomp
-    puts (select_station.to_i >= 0 && select_station.to_i <= @station.length) ? "Region :#{station[select_station.to_i - 1]} \n\n": '[ERROR] Please select provided number above.'
+    if select_station.to_i >= 0 && select_station.to_i <= @station.length
+      puts 'Region :#{station[select_station.to_i - 1]} \n'
+    else
+      puts '[ERROR]'
+    end
     selectdropdown(select_station.to_i - 1)
   end
 
   def selectdropdown(num)
     url = 'http://www.hcbus.com.tw/big5/service.asp'
     agent = Mechanize.new
-    page = agent.get(url)
-    form = agent.page.forms.first
-    form.field_with(:name => 'jumpMenu').options[num.to_i].click
-    page2 = form.submit
-    @page = page2
+    form = agent.get(url).forms.first
+    form.field_with(name: 'jumpMenu').options[num.to_i].click
+    tmppage = form.submit
+    @page = tmppage
     getinformation
   end
 
   def getinformation
-    key = [], value = [],key2 = []
+    key = [], value = [], key2 = []
     title = @page.parser.xpath("//table/tr[@class='text1_white']/td")
     content = @page.parser.xpath("//table/tr/td[@class='map-style']")
     content2 = @page.parser.xpath("//table/tr/td[@class='map-style'][1]")
-    content2.each{|b|  key2 << b.text.strip}
-    title.each {|t| key << t.text.strip}
-    content.each {|c| value << c.text.strip}
-    makehash(key, value,key2)
+    content2.each { |b| key2 << b.text.strip }
+    title.each { |t| key << t.text.strip }
+    content.each { |c| value << c.text.strip }
+    makehash(key, value, key2)
+    filehash(key, value, key2)
   end
-  def makehash(key, value,key2)
-@output=[]
-    value.each do |v|
-             key2.each do |c|
-                 
-                  if(v==c)
-                    puts "**************************************"
-                @output << "**************************************"
-                    end
-                 end
-             puts v
-              @output << v
+
+  def makehash(_key, value, tmpkey)
+    value.each  do  |v|
+      tmpkey.each  do  |c|
+        puts '**************************************' if v == c
       end
-          puts"**************************************"
-                  @output << "**************************************"
+      puts v
+    end
+    puts '**************************************'
+  end
+
+  def filehash(_key, value, tmpkey)
+    @output = []
+    value.each  do  |v|
+      tmpkey.each  do  |c|
+        @output << '**************************************'  if v == c
+      end
+      @output << v
+    end
+    @output << '**************************************'
   end
 end
