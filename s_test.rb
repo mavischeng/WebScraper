89c68237a35b9332dd543ae04264c4654e2ccaa3
erinerin <jolin_2121@hotmail.com.tw>
2014-10-19 00:55:23 +0800

    run but still have problem

--- s_test.rb
+++ s_test.rb
@@ -4,18 +4,18 @@ require 'minitest/rg'
 require './scraper.rb'
 #require_relative 'executable'
 
-teststation = {
-'1' => '新竹地區',
-'2' => '竹東地區',
-'3' => '關西地區',
-'4' => '桃園地區',
-'5' => '苗栗地區',
-'6' => '國道班車資訊',
-'7' => '一般公路資訊',
-'8' => '新竹市區公車',
-'9' => '桃園縣市區公車資訊',
-'10' => '免費公車資訊'
-}
+teststation = [
+'新竹地區',
+'竹東地區',
+'關西地區',
+'桃園地區',
+'苗栗地區',
+'國道班車資訊',
+'一般公路資訊',
+'新竹市區公車',
+'桃園縣市區公車資訊',
+'免費公車資訊'
+]
 
 describe "station" do
 
@@ -24,7 +24,7 @@ before do
 end
 
 it 'same' do
-@found['1'].must_equal Data.parse(teststation['1'])
+@found[1].must_equal Data.parse(teststation[1])
 end
 end
 
