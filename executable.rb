c10fb0b94c0785557d5c08d7907b6e464d939cc7
erinerin <jolin_2121@hotmail.com.tw>
2014-10-18 23:18:31 +0800

    Update executable.rb
    TEST!!!!!!!!!!!!!!

--- executable.rb
+++ executable.rb
@@ -1,22 +1,96 @@
-require_relative 'scraper'
-usage = []
-usage << "\n Usage : executable [number] .\n"
-usage << "\nNumber 1 : Hsinchu bus\' stations."
-usage << "\n Number 2 : Get bus stataion names."
-usage << "\n Number 3 : Get bus stataion information in detail."
-usage << "\n Number 4 : Get file (bus stataion information)bus."
-fail ArgumentError, usage if ARGV.count != 1
-number = ARGV[0]
-scmachine = WebScraper.new
-case number
-when '1'
-  scmachine.getwebstructure('http://www.hcbus.com.tw')
-when '2'
-  scmachine.busstation
-when '3'
-  scmachine.selectstation
-when '4'
-  scmachine.file_output
-else
-  puts '(1~4) Error input : you key in No ' << number.to_s
+require 'open-uri'
+require 'nokogiri'
+require 'rubygems'
+require 'mechanize'
+# Class that can be used to grab data from website(http://www.hcbus.com.tw/)
+class WebScraper
+  # @data stores information
+  attr_accessor :data
+  attr_accessor :station
+  attr_accessor :page
+  attr_accessor :output
+url='http://www.hcbus.com.tw/big5/service.asp'
+  def initialize
+    @data = data
+    @station = station
+    @page = page
+    @output = output
+  end
+
+  def getwebstructure(website)
+    web_data = open(website)
+    @data = Nokogiri.HTML(web_data)
+    File.write('Structure.txt', @data)
+  end
+
+  def file_output
+    selectstation
+    File.write('Output.txt', @output)
+    puts "\n\n\nPlease check data in Output.txt file"
+  end
+
+  def busstation
+    num, @station = 1, []
+    getwebstructure(url)
+    @data.css("select[name='jumpMenu'] option").each do |x|
+    @station << x.text  
+#return   x.text              
+    end
+  return   @station
+  end
+
+  def selectstation
+    busstation
+    puts 'Please select a region  (Enter:1 or 2 or 3.....)'
+    select_station = $stdin.gets.chomp
+    if select_station.to_i >= 0 && select_station.to_i <= @station.length
+      puts 'Region :#{station[select_station.to_i - 1]} \n'
+    else
+      puts '[ERROR]'
+    end
+    selectdropdown(select_station.to_i - 1)
+  end
+
+  def selectdropdown(num)
+    #url = 'http://www.hcbus.com.tw/big5/service.asp'
+    agent = Mechanize.new
+    form = agent.get(url).forms.first
+    form.field_with(name: 'jumpMenu').options[num.to_i].click
+    tmppage = form.submit
+    @page = tmppage
+    getinformation
+  end
+
+  def getinformation
+    key = [], value = [], key2 = []
+    title = @page.parser.xpath("//table/tr[@class='text1_white']/td")
+    content = @page.parser.xpath("//table/tr/td[@class='map-style']")
+    content2 = @page.parser.xpath("//table/tr/td[@class='map-style'][1]")
+    content2.each { |b| key2 << b.text.strip }
+    title.each { |t| key << t.text.strip }
+    content.each { |c| value << c.text.strip }
+    makehash(key, value, key2)
+    filehash(key, value, key2)
+  end
+
+  def makehash(_key, value, tmpkey)
+    value.each  do  |v|
+      tmpkey.each  do  |c|
+        puts '**************************************' if v == c
+      end
+      puts v
+    end
+    puts '**************************************'
+  end
+
+  def filehash(_key, value, tmpkey)
+    @output = []
+    value.each  do  |v|
+      tmpkey.each  do  |c|
+        @output << '**************************************'  if v == c
+      end
+      @output << v
+    end
+    @output << '**************************************'
+  end
 end
