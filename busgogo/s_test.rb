77ea90ec4e0fbf4f72c0f94a1757bdf9576e5fa5
wei <wei.yen.0718@gmail.com>
2014-10-19 10:45:33 +0800

    merge

---
+++ busgogo/s_test.rb
@@ -0,0 +1,46 @@
+#!/bin/env ruby
+# encoding: utf-8
+
+require 'minitest/autorun'
+require 'minitest/rg'
+require './scraper.rb'
+
+teststation = {
+'1' => "新竹地區",
+'2' => "竹東地區",
+'3' => "關西地區",
+'4' => "桃園地區",
+'5' => "苗栗地區",
+'6' => "國道班車資訊",
+'7' => "一般公路資訊",
+'8' => "新竹市區公車",
+'9' => "桃園縣市區公車資訊",
+'10' => "免費公車資訊"
+}
+
+describe "station" do
+
+before do
+mac= WebScraper.new
+ @found= mac.busstation
+#@selet=mac.tmp_selectstation
+end
+
+it 'find 新竹地區' do
+@found[1].must_equal teststation['1']
+end
+
+it 'find 竹地區' do
+@found[2].must_equal teststation['2']
+end
+
+it 'find 苗栗地區' do
+@found[5].must_equal teststation['5']
+end
+
+it 'find 桃園縣市區公車資訊' do
+@found[9].must_equal teststation['9']
+end
+end
+
+
